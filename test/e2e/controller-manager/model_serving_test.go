16b4c518392c8d26b427b71302800acf9e7bfb94
LiZhenCheng9527 <lizhencheng6@huawei.com>
2026-01-16 14:46:17 +0800

    add an e2e test example of modelserving
    Signed-off-by: LiZhenCheng9527 <lizhencheng6@huawei.com>

--- test/e2e/controller-manager/model_serving_test.go
+++ test/e2e/controller-manager/model_serving_test.go
@@ -19,15 +19,103 @@ package controller_manager
 import (
 	"testing"
 
-	workload "github.com/volcano-sh/kthena/pkg/apis/workload/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	workload "github.com/volcano-sh/kthena/pkg/apis/workload/v1alpha1"
+	"github.com/volcano-sh/kthena/test/e2e/utils"
 )
 
 // TODO(user): Add E2E tests for ModelServing lifecycle (Create, Update, Delete and check if it works)
 func TestModelServingLifecycle(t *testing.T) {
 	// Placeholder for ModelServing lifecycle tests
 	t.Skip("ModelServing lifecycle test is not implemented yet")
+}
+
+// TestModelServingScaleUp tests the ability to scale up a ModelServing's ServingGroup
+func TestModelServingScaleUp(t *testing.T) {
+	ctx, kthenaClient := setupControllerManagerE2ETest(t)
+
+	// Create a basic ModelServing with 1 replica
+	modelServing := createBasicModelServing("test-scale-up", 1, 1)
+
+	// Create the ModelServing
+	t.Log("Creating ModelServing with 1 servingGHroup replica")
+	_, err := kthenaClient.WorkloadV1alpha1().ModelServings(testNamespace).Create(ctx, modelServing, metav1.CreateOptions{})
+	require.NoError(t, err, "Failed to create ModelServing")
+
+	// Wait for the initial ModelServing to be ready
+	t.Log("Waiting for initial ModelServing (1 servingGroup replica) to be ready")
+	utils.WaitForModelServingReady(t, ctx, kthenaClient, testNamespace, modelServing.Name)
+
+	// Verify initial state - should have 1 replica
+	initialMS, err := kthenaClient.WorkloadV1alpha1().ModelServings(testNamespace).Get(ctx, modelServing.Name, metav1.GetOptions{})
+	require.NoError(t, err, "Failed to get initial ModelServing")
+	assert.Equal(t, int32(1), *initialMS.Spec.Replicas, "Initial ModelServing should have 1 replica")
+	assert.Equal(t, int32(1), initialMS.Status.AvailableReplicas, "Initial ModelServing should have 1 available replica")
+
+	// Update the ModelServing to scale up to 3 replicas
+	scaleUpMS := initialMS.DeepCopy()
+	newReplicas := int32(3)
+	scaleUpMS.Spec.Replicas = &newReplicas
+
+	t.Log("Updating ModelServing to scale up to 3 replicas")
+	updatedMS, err := kthenaClient.WorkloadV1alpha1().ModelServings(testNamespace).Update(ctx, scaleUpMS, metav1.UpdateOptions{})
+	require.NoError(t, err, "Failed to update ModelServing for scale up")
+
+	// Verify the spec was updated
+	assert.Equal(t, int32(3), *updatedMS.Spec.Replicas, "Updated ModelServing should have 3 replicas in spec")
+
+	// Wait for the scaled-up ModelServing to be ready
+	t.Log("Waiting for scaled-up ModelServing (3 replicas) to be ready")
+	utils.WaitForModelServingReady(t, ctx, kthenaClient, testNamespace, updatedMS.Name)
+
+	// Final verification - should have 3 replicas
+	finalMS, err := kthenaClient.WorkloadV1alpha1().ModelServings(testNamespace).Get(ctx, updatedMS.Name, metav1.GetOptions{})
+	require.NoError(t, err, "Failed to get final ModelServing")
+	assert.Equal(t, int32(3), *finalMS.Spec.Replicas, "Final ModelServing should have 3 replicas in spec")
+	assert.Equal(t, int32(3), finalMS.Status.AvailableReplicas, "Final ModelServing should have 3 available replicas")
+
+	t.Log("ModelServing scale up test passed successfully")
+}
+
+func createBasicModelServing(name string, servingGroupReplicas, roleReplicas int32) *workload.ModelServing {
+	return &workload.ModelServing{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: testNamespace,
+		},
+		Spec: workload.ModelServingSpec{
+			Replicas: &servingGroupReplicas,
+			Template: workload.ServingGroup{
+				Roles: []workload.Role{
+					{
+						Name:     "prefill",
+						Replicas: &roleReplicas,
+						EntryTemplate: workload.PodTemplateSpec{
+							Spec: corev1.PodSpec{
+								Containers: []corev1.Container{
+									{
+										Name:  "test-container",
+										Image: "nginx:latest",
+										Ports: []corev1.ContainerPort{
+											{
+												Name:          "http",
+												ContainerPort: 80,
+											},
+										},
+									},
+								},
+							},
+						},
+						WorkerReplicas: 0,
+					},
+				},
+			},
+		},
+	}
 }
 
 func createInvalidModelServing() *workload.ModelServing {
