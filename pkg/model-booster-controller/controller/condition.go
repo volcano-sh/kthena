f92e631ea2d110554542625508c361a03081891b
Vanshika <pahalvanshikaa@gmail.com>
2026-07-07 16:43:32 +0530

    style: fix gofmt import ordering in model-booster-controller
    gofmt requires imports within a block to be sorted alphabetically.
    Reorder imports in condition.go and model_booster_controller.go to
    satisfy gofmt -w with no behavioral change.
    
    Signed-off-by: Vanshika <pahalvanshikaa@gmail.com>

--- pkg/model-booster-controller/controller/condition.go
+++ pkg/model-booster-controller/controller/condition.go
@@ -19,8 +19,8 @@ package controller
 import (
 	"context"
 
-	msUtils "github.com/volcano-sh/kthena/pkg/model-serving-controller/utils"
 	workloadv1alpha1 "github.com/volcano-sh/kthena/pkg/apis/workload/v1alpha1"
+	msUtils "github.com/volcano-sh/kthena/pkg/model-serving-controller/utils"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
